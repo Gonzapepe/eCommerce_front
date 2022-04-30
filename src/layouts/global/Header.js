aa250cea49d18535f98658612af832e0d14d1ac5
Gonzaloaddamo <addamogonzalo@gmail.com>
2022-04-30 20:18:43 -0300

    Dropdown made

--- src/layouts/global/Header.js
+++ src/layouts/global/Header.js
@@ -1,8 +1,10 @@
 import Cookies from "js-cookie";
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as Profile } from "../../assets/icons/Profile.svg";
+import { ReactComponent as Settings } from "../../assets/icons/Settings.svg";
+import { ReactComponent as Logout } from "../../assets/icons/Logout.svg";
 import "./headerStyle.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Header = ({ user }) => {
   const navigate = useNavigate();
@@ -11,6 +13,8 @@ const Header = ({ user }) => {
   const logOut = () => {
     Cookies.remove("token");
   };
+
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="flex justify-center w-full h-24 bg-blue-700 text-white">
@@ -36,22 +40,50 @@ const Header = ({ user }) => {
             className="hidden sm:flex sm:flex-row sm:items-center sm:gap-x-6 "
             id="logIn_signUp"
           >
-            <div className="flex flex-row">
-              <Profile
-                width="25"
-                height="25"
-                className=" color-profile mr-1"
-                color="white"
-              />
-              <p>{user.name}</p>
+            <div className="dropdown relative">
+              {/* Dropdown button */}
+              <button
+                className=" flex flex-row dropdown-toggle hover:underline hover:opacity-80 bg-transparent"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                <Profile
+                  width="25"
+                  height="25"
+                  className=" color-profile mr-1"
+                  color="white"
+                />
+                <p>{user.name}</p>
+              </button>
+              {/* Menu item */}
+              {isOpen && (
+                <div className="z-10 origin-top-right absolute rounded-md -right-1 mt-2 w-40  shadow-lg bg-white divide-y divide-gray-100 focus:outline-none">
+                  <div className="">
+                    <Link
+                      to="/settings"
+                      className="group w-full flex items-center px-4 py-2 text-sm text-black hover:underline hover:opacity-60"
+                    >
+                      <Settings width="14" height="14" className="mr-1" />
+                      Configuración
+                    </Link>
+                    <button
+                      onClick={() => logOut()}
+                      className="group w-full flex items-center px-4 py-2 text-sm text-black hover:underline hover:opacity-60 "
+                    >
+                      <Logout width="14" height="14" className="mr-1" />
+                      Cerrar sesión
+                    </button>
+                  </div>
+                </div>
+              )}
             </div>
 
+            {/*             
             <button
               className="bg-cyan-600 rounded-lg p-2 hover:opacity-90 "
               onClick={logOut}
             >
               Cerrar sesión
-            </button>
+            </button> */}
           </div>
         ) : (
           <div
