162ce8b0c66cdb038e416b5e6626ac1becf3525c
Gonzaloaddamo <addamogonzalo@gmail.com>
2022-06-16 22:21:30 -0300

    making a modal

--- src/components/Dashboard/EditProduct.jsx
+++ src/components/Dashboard/EditProduct.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import DashboardSidebar from "../../layouts/DashboardSidebar/DashboardSidebar";
 import { fetchProducts } from "../../redux/reducers/products/products.actions";
 import Spinner from "../Spinner/Spinner";
+import EditProductModal from "../../layouts/EditProductModal/EditProductModal";
+import { ReactComponent as Edit } from "../../assets/icons/Edit.svg";
+import { ReactComponent as Trash } from "../../assets/icons/trash.svg";
 
 const EditProduct = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
   const { products, isLoading } = useSelector((state) => state.products);
   let subcategories = [];
   useEffect(() => {
@@ -28,19 +32,19 @@ const EditProduct = () => {
       </aside>
       <div className="m-auto flex align-center justify-between w-9/12 h-screen ">
         <div className="m-auto ">
-          <table className="w-full">
+          <table className="w-full bg-white">
             <thead className="">
               <tr>
-                <th className="w-1/5 text-center border border-gray-400 font-normal px-2 py-2">
+                <th className="w-1/5 font-bold text-center border border-gray-400  px-2 py-2">
                   Categoría
                 </th>
-                <th className="w-1/5 text-center border border-gray-400 font-normal px-2 py-2">
+                <th className="w-1/5 font-bold text-center border border-gray-400  px-2 py-2">
                   Subcategoría(s)
                 </th>
-                <th className="w-2/5 text-center border border-gray-400 font-normal px-2 py-2">
+                <th className="w-2/5 font-bold text-center border border-gray-400  px-2 py-2">
                   Nombre
                 </th>
-                <th className="w-1/ text-center border border-gray-400 font-normal px-2 py-2">
+                <th className="w-1/ font-bold text-center border border-gray-400  px-2 py-2">
                   Editar
                 </th>
               </tr>
@@ -54,14 +58,25 @@ const EditProduct = () => {
                   {product.subcategories.forEach((subcategory) => {
                     subcategories.push(subcategory.name);
                   })}
-                  <td className="text-center border-gray-400 border-r">
+                  <td className="text-center border-gray-400 ">
                     {product.category}
                   </td>
-                  <td className="text-center border-gray-400 border-r">
+                  <td className="text-center border-gray-400 ">
                     {subcategories.join(",")}
                   </td>
-                  <td className="text-center border-gray-400 border-r">
+                  <td className="text-center border-gray-400 ">
                     {product.title}
+                  </td>
+                  <td className="flex  justify-center items-center h-20">
+                    <button
+                      className="rounded bg-blue-500 p-2 mr-2 "
+                      onClick={() => setIsOpen(!isOpen)}
+                    >
+                      <Edit width="14" height="14" />
+                    </button>
+                    <button className="rounded bg-red-500 p-2 ">
+                      <Trash width="14" height="14" />
+                    </button>
                   </td>
                 </tr>
               ))}
@@ -69,6 +84,7 @@ const EditProduct = () => {
           </table>
         </div>
       </div>
+      {isOpen && <EditProductModal />}
     </div>
   );
 };
---
+++ src/layouts/EditProductModal/EditProductModal.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchProduct } from "../../redux/reducers/product/product.actions";
+import FileInput from "../../layouts/FileInput/FileInput";
+
+const EditProductModal = ({ id }) => {
+  const [formData, setFormData] = useState({});
+  const { product } = useSelector((state) => state.product);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProduct(id));
+  }, [dispatch, id]);
+
+  useEffect(() => {
+    if (formData.length === undefined || formData.length === null) {
+      setFormData({
+        title: product.title,
+        description: product.description,
+        stock: product.stock,
+        price: product.price,
+        category: product.category,
+        images: product.images,
+      });
+    }
+  }, [product]);
+
+  const onFileChange = (files) => {
+    console.log(files);
+    setFormData((values) => {
+      return { ...values, images: files };
+    });
+  };
+
+  const handleChange = ({ target: { name, value } }) => {
+    setFormData((values) => {
+      return { ...values, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="absolute flex w-screen h-screen bg-black/75">
+      <div className="z-50 overflow-hidden w-2/4 m-auto align-center bg-white accent-white rounded">
+        <form
+          onSubmit={handleSubmit}
+          className="m-auto w-10/12 accent-white p-5"
+        >
+          <div className="header">
+            <p className="font-semibold text-xl">Editar producto</p>
+          </div>
+          {/* Titulo del producto */}
+          <div className="mt-3">
+            <label for="title" className="font-semibold text-base">
+              Nombre del producto
+            </label>
+            <input
+              onChange={(e) => handleChange(e)}
+              placeholder="título"
+              type="text"
+              id="title"
+              name="title"
+              className="text-base w-full bg-clip-padding bg-transparent border-0 border-b border-solid border-slate-700 pt-1 px-0 focus:outline-none focus:border-solid focus:border-sky-700"
+            />
+          </div>
+
+          {/* Descripción del producto */}
+          <div className="mt-3">
+            <label for="description" className="font-semibold text-base">
+              Descripción
+            </label>
+            <input
+              onChange={(e) => handleChange(e)}
+              type="text"
+              id="description"
+              name="description"
+              placeholder="Descripción"
+              className="text-base w-full bg-clip-padding bg-transparent border-0 border-b border-solid border-slate-700 pt-1 px-0 focus:outline-none focus:border-solid focus:border-sky-700"
+            />
+          </div>
+
+          {/* Stock y precio */}
+          <div className="mt-3 flex flex-row justify-between">
+            <div className="mr-3">
+              <label for="stock" className="font-semibold text-base">
+                Stock
+              </label>
+              <input
+                onChange={(e) => handleChange(e)}
+                type="number"
+                id="stock"
+                name="stock"
+                placeholder="Stock"
+                className="text-base w-full bg-clip-padding bg-transparent border-0 border-b border-solid border-slate-700 pt-1 px-0 focus:outline-none focus:border-solid focus:border-sky-700"
+              />
+            </div>
+            <div className="ml-3">
+              <label for="stock" className="font-semibold text-base">
+                Precio
+              </label>
+              <input
+                onChange={(e) => handleChange(e)}
+                type="text"
+                id="price"
+                name="price"
+                placeholder="Precio"
+                className="text-base w-full bg-clip-padding bg-transparent border-0 border-b border-solid border-slate-700 pt-1 px-0 focus:outline-none focus:border-solid focus:border-sky-700"
+              />
+            </div>
+          </div>
+
+          {/* Categoría */}
+          <div className="mt-3 flex flex-col">
+            <label for="category" className="font-semibold text-base">
+              Categoría
+            </label>
+            <select
+              onChange={(e) => handleChange(e)}
+              name="category"
+              id="category"
+              className="mt-2 w-30 text-center border p-1 rounded"
+            >
+              <option value={null}>Elegir categoría</option>
+              <option value={"muebles"}>Muebles</option>
+              <option value={"pisos"}>Pisos</option>
+              <option value={"sanitarios"}>Sanitarios</option>
+              <option value={"cocina"}>Cocina</option>
+              <option value={"accesorios"}>Accesorios</option>
+              <option value={"pinturas"}>Pinturas</option>
+            </select>
+          </div>
+
+          {/* Subir imagen */}
+          <div className="mt-5 flex items-center justify-center">
+            <FileInput onFileChange={(files) => onFileChange(files)} />
+          </div>
+
+          {/* Botones */}
+          <div className="flex flex-row justify-end mt-5">
+            <button className="text-blue-500 rounded shadow-md py-2 px-4 ml-2">
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className="ml-5 text-white bg-blue-500 rounded shadow-md py-2 px-4 ml-2"
+            >
+              Añadir
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditProductModal;
