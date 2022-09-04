60776e7161a03de456f2f4d9bc5cfea676773470
Gonzaloaddamo <addamogonzalo@gmail.com>
2022-09-04 20:44:30 -0300

    Pagination loads but render item not working

--- src/layouts/pagination/pagination.js
+++ src/layouts/pagination/pagination.js
@@ -7,37 +7,10 @@ import PaginationItem from "@material-ui/lab/PaginationItem";
 const MainPagination = (props) => {
   let { path, pagesCount } = props;
   const location = useLocation().search;
+  const page = location ? location : 1;
   return (
-    <div className="general-pagination">
-      <Routes>
-        <Route path={path}>
-          {() => {
-            console.log("LOCATION FROM MAIN PAGINATION: ", location);
-            // const page = parseInt(query.get("page") || "1", 10);
-            const page = location ? location : 1;
-            return (
-              <Pagination
-                variant="outlined"
-                page={page}
-                count={pagesCount}
-                renderItem={(item) => {
-                  console.log("ITEM: ", item);
-                  return (
-                    <PaginationItem
-                      component={Link}
-                      to={`${
-                        path + item.page === 1 ? "" : `?page=${item.page}`
-                      }`}
-                      {...item}
-                    />
-                  );
-                }}
-              />
-            );
-          }}
-        </Route>
-      </Routes>
-    </div>
+    // const page = parseInt(query.get("page") || "1", 10);
+    <Pagination variant="outlined" page={page} count={pagesCount} />
   );
 };
 
