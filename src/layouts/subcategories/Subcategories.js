0677aa3702e2ff8a3aff33ab2c82cda1805edbef
Gonzaloaddamo <addamogonzalo@gmail.com>
2023-07-09 03:17:07 -0300

    feat: added search location to fetch subcategories

--- src/layouts/subcategories/Subcategories.js
+++ src/layouts/subcategories/Subcategories.js
@@ -1,16 +1,18 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSubcategories } from "../../redux/reducers/subcategories/subcategories.actions";
+import { useLocation } from "react-router-dom";
 
 const Subcategory = ({ parentCallback }) => {
   const dispatch = useDispatch();
   const { subcategories, isLoading } = useSelector(
     (state) => state.subcategories
   );
+  const search = useLocation().search;
 
   useEffect(() => {
-    dispatch(fetchSubcategories());
-  }, [dispatch]);
+    dispatch(fetchSubcategories(search));
+  }, [dispatch, search]);
 
   const handleToggle = (name) => {
     parentCallback(name);
@@ -27,25 +29,26 @@ const Subcategory = ({ parentCallback }) => {
         Subcategorías{" "}
       </h2>
       <div className="mt-4 flex flex-col">
-        {subcategories.map((subcategory) => {
-          console.log(subcategory);
-          return (
-            <div key={subcategory.id} className="inline-block ml-10">
-              <input
-                className="mr-1 align-middle"
-                type="checkbox"
-                id={subcategory.id}
-                name={subcategory.name}
-                value={subcategory.name}
-                onChange={() => handleToggle(subcategory.name)}
-              />
-              <label for={subcategory.id} className="align-middle">
-                {" "}
-                {subcategory.name}{" "}
-              </label>
-            </div>
-          );
-        })}
+        {subcategories.subcategories &&
+          subcategories.subcategories.map((subcategory) => {
+            console.log(subcategory);
+            return (
+              <div key={subcategory.id} className="inline-block ml-10">
+                <input
+                  className="mr-1 align-middle"
+                  type="checkbox"
+                  id={subcategory.id}
+                  name={subcategory.name}
+                  value={subcategory.name}
+                  onChange={() => handleToggle(subcategory.name)}
+                />
+                <label for={subcategory.id} className="align-middle">
+                  {" "}
+                  {subcategory.name}{" "}
+                </label>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
